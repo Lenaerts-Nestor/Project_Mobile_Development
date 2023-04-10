31bd530ca1284a0341c495fb8f775c68a813eec9
Nestor Lenaerts <s122452@ap.be>
2023-04-10 14:13:02 +0200

    model aangemaakt

--- lib/model/favorietModel.dart
+++ lib/model/favorietModel.dart
@@ -1,1 +1,9 @@
-class Favorite {}
+// ignore_for_file: file_names
+
+class Favorite {
+  final bool status; //beschikbaar waar/onwaar.
+  final String streetname; // ellermanstraat.
+  final String municipality; //gemeente deurne.
+
+  Favorite(this.status, this.streetname, this.municipality);
+}
