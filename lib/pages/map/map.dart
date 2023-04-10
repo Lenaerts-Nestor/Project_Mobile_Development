6a51dfcd8b29f01f4e5eef8be80ed79487f67257
Nestor Lenaerts <s122452@ap.be>
2023-04-10 14:59:37 +0200

    user halen van database

--- lib/pages/map/map.dart
+++ lib/pages/map/map.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MapPage extends StatelessWidget {
@@ -5,9 +6,10 @@ class MapPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
-      body: const Center(
-        child: Text('Map'),
+      body: Center(
+        child: Text(user.uid),
       ),
     );
   }
