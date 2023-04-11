9895496c78db3a743a666e00554b058239e369f2
Nestor Lenaerts <s122452@ap.be>
2023-04-11 21:55:35 +0200

    alle user tonen van de database testen fase 1

--- lib/model/userModel.dart
+++ lib/model/userModel.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: file_names
 
+import 'package:firebase_auth/firebase_auth.dart';
+
 class User_account {
   String id;
   final String name;
@@ -13,6 +15,14 @@ class User_account {
     required this.password,
   });
 
+  //dit is om info te schrijven naar database
   Map<String, dynamic> tojson() =>
       {'id': id, 'name': name, 'email': email, 'password': password};
+
+  //aanvragen informatie van de database.
+  static User_account fromJson(Map<String, dynamic> json) => User_account(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      password: json['password']);
 }
--- lib/pages/map/map.dart
+++ lib/pages/map/map.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:parkflow/model/userModel.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -8,9 +10,31 @@ class MapPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
-      body: Center(
-        child: Text(user.uid),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('map'),
+        ),
+        body: StreamBuilder<List<User_account>>(
+          stream: readUsers(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final users = snapshot.data!;
+              return ListView(children: users.map(buildUsers).toList());
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ));
   }
+
+  //info lezen van database
+  Stream<List<User_account>> readUsers() =>
+      FirebaseFirestore.instance.collection('users').snapshots().map((event) =>
+          event.docs.map((e) => User_account.fromJson(e.data())).toList());
+
+  // testen om info af te halen van database alle users.
+  Widget buildUsers(User_account user) => ListTile(
+        leading: CircleAvatar(child: Text('${user.id}')),
+        title: Text(user.name),
+        subtitle: Text(user.email),
+      );
 }
