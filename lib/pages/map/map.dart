35f84e5b47c8098aec1303d8d6328f80c282f713
Nestor Lenaerts <s122452@ap.be>
2023-04-18 19:21:02 +0200

    map update v2 met pop up methode

--- lib/pages/map/map.dart
+++ lib/pages/map/map.dart
@@ -11,13 +11,19 @@ class MapPage extends StatefulWidget {
   _MapPageState createState() => _MapPageState();
 }
 
+//nog te doen:
+//1- markers verwijderen.
+//2- markers toevoegen [knop links beneden doen ?] \figma updaten?
+//3- markers informatie bewaren.
+
 class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
       options: MapOptions(
+        //begin locatie - vragen aan meneer als het het vaste plaats gaat zijn of user location
+        //als optie 2 => uitvinden hoe je dat met andriod device doet. te veel errors.
         center: LatLng(51.2172, 4.4212),
-        //aangezien we niet gaan user location gebruiken maar een vaste plaats. we pakken central station.
         zoom: 16,
       ),
       children: [
@@ -33,8 +39,10 @@ class _MapPageState extends State<MapPage> {
               height: 80,
               builder: (context) => GestureDetector(
                 onTap: () {
-                  //werkt :D
-                  print('der is geklicked');
+                  // toon de methode.
+                  _showPopup(context);
+                  //onderzoeken hoe ik informatie bewaar van de marker.
+                  //classe mischien maken van markers of collection op firebase
                 },
                 child: const Icon(
                   Icons.ac_unit_outlined,
@@ -46,6 +54,59 @@ class _MapPageState extends State<MapPage> {
           ],
         ),
       ],
+    );
+  }
+
+//methode om de pop up the tonen.
+  void _showPopup(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      //voor een of andere reden laat het true [isScrollcontrolled], anders komt die pop up maar tot 40%. onderzoek waarom of zo laten.
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Container(
+          height: MediaQuery.of(context).size.height * 0.7,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15),
+              topRight: Radius.circular(15),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //mischien underline ? figma updaten ?
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    //momenteel dit text, later naam van iets ? locatie straat of ????
+                    const Text('Dit is de pop-up',
+                        style: TextStyle(fontSize: 24)),
+                    //meer centreren? center widget werk niet. onderzoeken!.
+                    IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: const Icon(Icons.close),
+                      iconSize: 30,
+                    ),
+                  ],
+                ),
+                //vind de lijn mooi.
+                const Divider(
+                  color: Colors.black,
+                  thickness: 1,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+                //sebiet dingens toevoegen hier? locatie en uren ?
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
