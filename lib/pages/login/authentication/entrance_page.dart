58c69046e70f5ff417007e33f0c32955f8cb3d91
Nestor Lenaerts <s122452@ap.be>
2023-04-20 22:49:46 +0200

    firebase AUTH verwijderd+sharepreferenstoegevoegd

--- lib/pages/login/authentication/entrance_page.dart
+++ lib/pages/login/authentication/entrance_page.dart
@@ -1,24 +1,35 @@
-// ignore_for_file: prefer_const_constructors, file_names
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:parkflow/pages/home/home_page.dart';
 import 'package:parkflow/pages/login/authentication/login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
+class entrance_page extends StatefulWidget {
+  const entrance_page({super.key});
 
   @override
-  Widget build(BuildContext context) => (Scaffold(
-        body: StreamBuilder<User?>(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return HomePage();
-            } else {
-              return AuthPage();
-            }
-          },
-        ),
-      ));
+  _entrance_pageState createState() => _entrance_pageState();
+}
+
+class _entrance_pageState extends State<entrance_page> {
+  bool _isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    checkLoggedInStatus();
+  }
+
+  Future<void> checkLoggedInStatus() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final loggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+    setState(() {
+      _isLoggedIn = loggedIn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: _isLoggedIn ? HomePage() : LoginPage(),
+      );
 }
