2e3765bd930fa93271cec5c31500b70206ac2d85
Nestor Lenaerts <s122452@ap.be>
2023-04-20 19:11:44 +0200

    updated sign_out_button met sharedpreferences.

--- lib/components/sign_out_button.dart
+++ lib/components/sign_out_button.dart
@@ -1,7 +1,8 @@
-// sign_out_button.dart
-import 'package:firebase_auth/firebase_auth.dart';
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'style/designStyle.dart' as myFontstyle;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SignOutButton extends StatelessWidget {
   const SignOutButton({Key? key}) : super(key: key);
@@ -9,12 +10,22 @@ class SignOutButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
-      onPressed: () => FirebaseAuth.instance.signOut(),
+      onPressed: () => signOut(context),
       icon: const Icon(Icons.arrow_back),
       label: Text(
-        'Sign Out',
+        'Log uit',
         style: TextStyle(fontSize: myFontstyle.fontSize1),
       ),
     );
+  }
+
+  Future<void> signOut(BuildContext context) async {
+    // update de locale data zodat het weet dat het uigelogd is, anders zieke buggs......
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isLoggedIn', false);
+    await prefs.remove('userId');
+
+    // ga naar de login page =>
+    Navigator.pushReplacementNamed(context, '/login');
   }
 }
