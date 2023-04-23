c6ed967b8633257654e3a0eee6661b17c17e8e29
Nestor Lenaerts <s122452@ap.be>
2023-04-24 00:28:42 +0200

    bluawe error weg doen

--- lib/pages/login-register/auth_services.dart
+++ lib/pages/login-register/auth_services.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously, avoid_print
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crypto/crypto.dart';
