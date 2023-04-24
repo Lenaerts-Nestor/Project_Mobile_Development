b1bc66246a30030cf43d466e58e135fecd07da38
Kevin Van Rooy <kevinvanrooy96@hotmail.com>
2023-04-24 14:20:08 +0200

    update nieuw design variabelen

--- lib/components/style/designStyle.dart
+++ lib/components/style/designStyle.dart
@@ -1,10 +1,24 @@
 // ignore_for_file: file_names
 
+//These are the variables assuming we're using a 400x844px screen
+
 // font_style.dart
+import 'package:flutter/material.dart';
 
-double fontSize1 = 80;
-double fontSize2 = 15;
-double fontSize3 = 10;
-
+double fontSize1 = 10;
+double fontSize2 = 20;
+double fontSize3 = 30;
 
 // font_family.dart
+String fontFamilyTitle = "Intro";
+String fontStyleTitle = "Medium";
+String fontFamilyText = "Urbane Rounded";
+String fontStyleText = "Medium";
+
+// colors
+const Color color1 = Color(0xFFFFFFFF);
+const Color color2 = Color(0xFFD5EFEE);
+const Color color3 = Color(0xFF8FE2DF);
+const Color color4 = Color(0xFF67D1D6);
+const Color color5 = Color(0xFF2D8E89);
+const Color color6 = Color(0xFF0F2D2B);
