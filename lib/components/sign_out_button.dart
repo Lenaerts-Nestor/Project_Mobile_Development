78eec2ec52b847db4583263c5c634157880d5561
Nestor Lenaerts <s122452@ap.be>
2023-04-27 11:20:32 +0200

    onodige code wegdoen op basis van dpedency.

--- lib/components/sign_out_button.dart
+++ lib/components/sign_out_button.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 
 import 'style/designStyle.dart' as myFontstyle;
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SignOutButton extends StatelessWidget {
   const SignOutButton({Key? key}) : super(key: key);
@@ -22,8 +21,5 @@ class SignOutButton extends StatelessWidget {
 
   Future<void> signOut(BuildContext context) async {
     // update de locale data zodat het weet dat het uigelogd is, anders zieke buggs......
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isLoggedIn', false);
-    await prefs.remove('userId');
   }
 }
