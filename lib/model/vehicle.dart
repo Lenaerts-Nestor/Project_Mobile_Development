b3c6819362621a0570abb315b62fbcc04be2e6af
Nestor Lenaerts <s122452@ap.be>
2023-05-04 19:24:03 +0200

    resevatie systeem upgedate en condities gemaakt.

--- lib/model/vehicle.dart
+++ lib/model/vehicle.dart
@@ -3,19 +3,21 @@ class Vehicle {
   final String model;
   final String brand;
   final String color;
+  late final bool availability;
 
-  Vehicle({
-    required this.id,
-    required this.model,
-    required this.brand,
-    required this.color,
-  });
+  Vehicle(
+      {required this.id,
+      required this.model,
+      required this.brand,
+      required this.color,
+      required this.availability});
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'model': model,
         'brand': brand,
         'kleur': color,
+        'beschikbaar': availability,
       };
 
   static Vehicle fromJson(Map<String, dynamic> json) => Vehicle(
@@ -23,5 +25,6 @@ class Vehicle {
         model: json['model'] as String,
         brand: json['brand'] as String,
         color: json['kleur'] as String,
+        availability: json['beschikbaar'] as bool,
       );
 }
--- lib/pages/map/map_functions.dart
+++ lib/pages/map/map_functions.dart
@@ -6,10 +6,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:parkflow/components/custom_button.dart';
+import 'package:parkflow/components/custom_dropdown.dart';
+import 'package:parkflow/model/user/user_account.dart';
+import 'package:parkflow/model/vehicle.dart';
 import 'package:provider/provider.dart';
 import '../../model/user/user_logged_controller.dart';
 import 'package:intl/intl.dart';
 import 'package:parkflow/components/style/designStyle.dart';
+
+import '../../model/user/user_service.dart';
+import '../settings/pages/vehicles/addVehiclesPage.dart';
 
 final _firestore = FirebaseFirestore.instance;
 
@@ -26,34 +32,41 @@ void getMarkersFromDatabase(BuildContext context,
   final markersSnapshot = await _firestore.collection('markers').get();
   List<Marker> markers = markersSnapshot.docs.map((doc) {
     LatLng latLng = LatLng(doc['latitude'], doc['longitude']);
-    String userId = doc['userId'];
+    String reservedId = doc['reservedId'];
     DateTime startTime = doc['startTime'].toDate();
     DateTime endTime = doc['endTime'].toDate();
     DateTime prevEndTime = doc['prevEndTime'].toDate();
     bool isGreenMarker = doc['isGreenMarker'];
-    return createMarkersFromDatabase(context, latLng, userId, startTime,
+    return createMarkersFromDatabase(context, latLng, reservedId, startTime,
         endTime, prevEndTime, isGreenMarker);
   }).toList();
   onMarkersFetched(markers);
 }
 
-void createMarker(LatLng latlng, String userId, BuildContext context,
+void createMarker(LatLng latlng, String reservedId, BuildContext context,
     void Function(Marker newMarker) onMarkerCreated) {
   DateTime startTime = DateTime.now();
   DateTime endTime = DateTime.now().add(const Duration(minutes: 1));
   DateTime prevEndTime = startTime;
   bool isGreenMarker = true;
   saveMarkerToDatabase(
-      latlng, userId, startTime, endTime, prevEndTime, isGreenMarker);
-  Marker newMarker = createMarkersFromDatabase(
-      context, latlng, userId, startTime, endTime, prevEndTime, isGreenMarker);
+      //TODO: parked ID toevoegen
+      latlng,
+      reservedId,
+      '',
+      startTime,
+      endTime,
+      prevEndTime,
+      isGreenMarker);
+  Marker newMarker = createMarkersFromDatabase(context, latlng, reservedId,
+      startTime, endTime, prevEndTime, isGreenMarker);
   onMarkerCreated(newMarker);
 }
 
 Marker createMarkersFromDatabase(
     BuildContext context,
     LatLng latlng,
-    String userId,
+    String reservedId,
     DateTime startTime,
     DateTime endTime,
     DateTime prevEndTime,
@@ -64,7 +77,7 @@ Marker createMarkersFromDatabase(
 
   if (isGreenMarker) {
     markerColor = Colors.green;
-  } else if (userEmail == userId) {
+  } else if (userEmail == reservedId) {
     markerColor = Colors.blue;
   } else {
     markerColor = Colors.black;
@@ -76,12 +89,13 @@ Marker createMarkersFromDatabase(
     point: latlng,
     builder: (ctx) => GestureDetector(
       onTap: () {
-        if (isGreenMarker && userEmail != userId) {
-          showPopupReserve(context, latlng, userId, startTime, endTime, true);
+        if (isGreenMarker && userEmail != reservedId) {
+          showPopupReserve(
+              context, latlng, reservedId, startTime, endTime, true);
         }
-        if (userEmail == userId) {
-          showPopupEdit(
-              context, latlng, userId, startTime, endTime, prevEndTime, false);
+        if (userEmail == reservedId) {
+          showPopupEdit(context, latlng, reservedId, startTime, endTime,
+              prevEndTime, false);
         }
       },
       child: Icon(Icons.location_on, color: markerColor, size: iconSizeNav),
@@ -89,9 +103,14 @@ Marker createMarkersFromDatabase(
   );
 }
 
+//TODO:
+// parked_user
+// reserved user
+
 Future<void> saveMarkerToDatabase(
   LatLng latlng,
-  String userId,
+  String reservedId,
+  String parkedId,
   DateTime startTime,
   DateTime endTime,
   DateTime prevEndTime,
@@ -110,7 +129,8 @@ Future<void> saveMarkerToDatabase(
   await _firestore.collection('markers').add({
     'latitude': latlng.latitude,
     'longitude': latlng.longitude,
-    'userId': userId,
+    'reservedId': reservedId,
+    'parkedId': parkedId,
     'startTime': startTime,
     'endTime': endTime,
     'prevEndTime': prevEndTime,
@@ -143,85 +163,170 @@ Future<void> updateMarkerState() async {
   }
 }
 
-void showPopupPark(BuildContext context, LatLng latLng, String userId) {
+Future<void> updateCarState() async {
+  try {
+    final docUser = FirebaseFirestore.instance.collection('users');
+    final markersSnapshot = await _firestore.collection('users').get();
+    for (var doc in markersSnapshot.docs) {
+      DateTime prevEndTime = doc['prevEndTime'].toDate();
+      if (prevEndTime.isBefore(DateTime.now())) {
+        await doc.reference.update({'isGreenMarker': true});
+      }
+    }
+  } catch (e) {
+    // Ignore the error and continue
+    print('Error occurred, but ignoring it: $e');
+  }
+}
+
+void showPopupPark(BuildContext context, LatLng latLng, String reservedId) {
+  late String selectedVehicle = '';
+
+  List<Vehicle> _vehicles = [];
+
   showModalBottomSheet(
     context: context,
     isScrollControlled: true,
     backgroundColor: Colors.transparent,
     builder: (BuildContext context) {
-      return StatefulBuilder(builder: (context, setState) {
-        DateTime endTime = DateTime.now().add(selectedTime);
-        return Container(
-          height: MediaQuery.of(context).size.height * 0.7,
-          decoration: const BoxDecoration(
-            color: color3,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(radius),
-              topRight: Radius.circular(radius),
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Testerstraat',
-                      style: TextStyle(fontSize: fontSize3),
+      return StreamBuilder<UserAccount>(
+        stream: readUserByLive(reservedId),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            final user = snapshot.data;
+            _vehicles = user!.vehicles;
+            if (user != null) {
+              final vehicles = user.vehicles
+                  .where((v) => v.availability)
+                  .toList(); // Filter vehicles with availability = true
+
+              bool buttonDisabled = vehicles.isEmpty;
+              if (selectedVehicle == '' && vehicles.isNotEmpty) {
+                selectedVehicle = vehicles.first.model;
+              }
+              return StatefulBuilder(builder: (context, setState) {
+                DateTime endTime = DateTime.now().add(selectedTime);
+                return Container(
+                  height: MediaQuery.of(context).size.height * 0.7,
+                  decoration: const BoxDecoration(
+                    color: color3,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(radius),
+                      topRight: Radius.circular(radius),
                     ),
-                    IconButton(
-                      onPressed: () => Navigator.pop(context),
-                      icon: const Icon(Icons.close),
-                      iconSize: iconSizeNav,
-                    ),
-                  ],
-                ),
-                const Divider(
-                  color: Colors.black,
-                  thickness: 1,
-                  indent: 20,
-                  endIndent: 20,
-                ),
-                const Text('Hoe lang gaat u parkeren?'),
-                SizedBox(
-                  height: 180,
-                  child: CupertinoDatePicker(
-                    initialDateTime: DateTime(0).add(selectedTime),
-                    mode: CupertinoDatePickerMode.time,
-                    use24hFormat: true,
-                    onDateTimeChanged: (DateTime value) {
-                      setState(() {
-                        selectedTime =
-                            Duration(hours: value.hour, minutes: value.minute);
-                      });
-                    },
                   ),
-                ),
-                const SizedBox(height: verticalSpacing2),
-                Text('van ${formatDateTime(DateTime.now())}'),
-                Text('tot  ${formatDateTime(endTime)}'),
-                const SizedBox(height: verticalSpacing2),
-                BlackButton(
-                  onPressed: () async {
-                    await saveMarkerToDatabase(latLng, userId, DateTime.now(),
-                        endTime, DateTime.now(), true);
-                    Navigator.pop(context);
-                  },
-                  text: 'parkeren',
-                ),
-              ],
-            ),
-          ),
-        );
-      });
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Testerstraat',
+                              style: TextStyle(fontSize: fontSize3),
+                            ),
+                            IconButton(
+                              onPressed: () => Navigator.pop(context),
+                              icon: const Icon(Icons.close),
+                              iconSize: iconSizeNav,
+                            ),
+                          ],
+                        ),
+                        const Divider(
+                          color: Colors.black,
+                          thickness: 1,
+                          indent: 20,
+                          endIndent: 20,
+                        ),
+                        const Text('Hoe lang gaat u parkeren?'),
+                        SizedBox(
+                          height: 180,
+                          child: CupertinoDatePicker(
+                            initialDateTime: DateTime(0).add(selectedTime),
+                            mode: CupertinoDatePickerMode.time,
+                            use24hFormat: true,
+                            onDateTimeChanged: (DateTime value) {
+                              setState(() {
+                                selectedTime = Duration(
+                                    hours: value.hour, minutes: value.minute);
+                              });
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: verticalSpacing2),
+                        Text('van ${formatDateTime(DateTime.now())}'),
+                        Text('tot  ${formatDateTime(endTime)}'),
+                        const SizedBox(height: verticalSpacing2),
+                        vehicles.isNotEmpty
+                            ? VehicleDropdown(
+                                items: vehicles
+                                    .map((vehicle) => vehicle.model)
+                                    .toList(),
+                                value: selectedVehicle,
+                                onChanged: (value) {
+                                  if (value == null) {
+                                    return;
+                                  }
+                                  setState(() {
+                                    selectedVehicle = value;
+                                    final selectedVehicleIndex =
+                                        _vehicles.indexWhere((vehicle) =>
+                                            vehicle.model == selectedVehicle);
+                                  });
+                                },
+                              )
+                            : const Text('voeg een vervoer toe aan je account'),
+                        const SizedBox(height: 80), //dit aangepast om te testen
+                        //knop mischien groter doen ???
+                        BlackButton(
+                          text: buttonDisabled ? 'ongeldige' : 'parkeren',
+                          isRed: buttonDisabled ? false : true,
+                          onPressed: buttonDisabled
+                              ? () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const AddVehicle()),
+                                  );
+                                }
+                              : () async {
+                                  final selectedVehicleIndex =
+                                      vehicles.indexWhere((vehicle) =>
+                                          vehicle.model == selectedVehicle);
+                                  if (selectedVehicleIndex >= 0) {
+                                    final selectedVehicle2 =
+                                        vehicles[selectedVehicleIndex];
+                                    await saveMarkerToDatabase(
+                                        latLng,
+                                        reservedId,
+                                        '', //TODO: parkedID doen
+                                        DateTime.now(),
+                                        endTime,
+                                        DateTime.now(),
+                                        true);
+                                    await toggleVehicleAvailability(
+                                        reservedId, selectedVehicle2);
+                                    Navigator.pop(context);
+                                  }
+                                },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              });
+            }
+          }
+          return const SizedBox.shrink();
+        },
+      );
     },
   );
 }
 
-void showPopupReserve(BuildContext context, LatLng latLng, String userId,
+void showPopupReserve(BuildContext context, LatLng latLng, String reservedId,
     DateTime startTime, DateTime endTime, bool isGreenMarker) {
   DateTime previousEndTime = endTime;
   showModalBottomSheet(
@@ -290,7 +395,8 @@ void showPopupReserve(BuildContext context, LatLng latLng, String userId,
                     if (isGreenMarker) {
                       final userLogged =
                           Provider.of<UserLogged>(context, listen: false);
-                      await saveMarkerToDatabase(latLng, userLogged.email,
+                      //Parked ID toevoegen
+                      await saveMarkerToDatabase(latLng, userLogged.email, '',
                           startTime, endTime, previousEndTime, false);
                     }
                     Navigator.pop(context);
@@ -309,7 +415,7 @@ void showPopupReserve(BuildContext context, LatLng latLng, String userId,
 void showPopupEdit(
     BuildContext context,
     LatLng latLng,
-    String userId,
+    String reservedId,
     DateTime startTime,
     DateTime endTime,
     DateTime previousEndTime,
@@ -383,7 +489,7 @@ void showPopupEdit(
                   onPressed: () async {
                     final userLogged =
                         Provider.of<UserLogged>(context, listen: false);
-                    await saveMarkerToDatabase(latLng, userLogged.email,
+                    await saveMarkerToDatabase(latLng, userLogged.email, '',
                         startTime, replacingEndTime, previousEndTime, true);
                     Navigator.pop(context);
                   },
