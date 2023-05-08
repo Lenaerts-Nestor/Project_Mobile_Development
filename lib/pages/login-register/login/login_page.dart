8179768f2cc21e6e1ed1791443bb8e3d1960f6d0
Nestor Lenaerts <s122452@ap.be>
2023-05-09 00:33:37 +0200

    login validators toegevoegd

--- lib/pages/login-register/login/login_page.dart
+++ lib/pages/login-register/login/login_page.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
 import 'package:parkflow/components/style/designStyle.dart';
 import 'package:parkflow/pages/login-register/auth_services.dart';
@@ -21,6 +19,22 @@ class _LoginPageState extends State<LoginPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+//validator gezet:
+  String? _emailValidator(String? value) {
+    if (value == null || !value.contains('@') || !value.contains('.')) {
+      return 'Invalid email';
+    }
+    return null;
+  }
+
+  String? _passwordValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required';
+    }
+    return null;
+  }
+
   @override
   void dispose() {
     emailController.dispose();
@@ -35,77 +49,86 @@ class _LoginPageState extends State<LoginPage> {
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(padding),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: verticalSpacing2*2),
-                Center(
-                  child: SvgPicture.asset(
-                    'assets/logo.svg',
-                    width: MediaQuery.of(context).size.width - padding*3,
-                  ),
-                ),
-                const SizedBox(height: verticalSpacing2*3),
-                const CustomText(
-                  text: 'Parkeren was nog nooit zo eenvoudig',
-                ),
-                const SizedBox(height: verticalSpacing2),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: TextField(
-                    controller: emailController,
-                    textInputAction: TextInputAction.next,
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
+            //voor validators:
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: verticalSpacing2 * 2),
+                  Center(
+                    child: SvgPicture.asset(
+                      'assets/logo.svg',
+                      width: MediaQuery.of(context).size.width - padding * 3,
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: verticalSpacing1,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: TextField(
-                    controller: passwordController,
-                    textInputAction: TextInputAction.done,
-                    decoration: const InputDecoration(
-                      labelText: 'Wachtwoord',
-                    ),
-                    obscureText: true,
+                  const SizedBox(height: verticalSpacing2 * 3),
+                  const CustomText(
+                    text: 'Parkeren was nog nooit zo eenvoudig',
                   ),
-                ),
-                const SizedBox(height: verticalSpacing2),
-                BlackButton(
-                  onPressed: () =>
-                      signin(context, emailController, passwordController),
-                  text: 'inloggen',
-                ),
-                CustomTextButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const ForgotPasswordPage(),
+                  const SizedBox(height: verticalSpacing2),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: TextFormField(
+                      controller: emailController,
+                      textInputAction: TextInputAction.next,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
                       ),
-                    );
-                  },
-                  text: 'Wachtwoord resetten',
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('geen account?'),
-                    CustomTextButton(
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (context) => const RegisterPage()),
-                        );
-                      },
-                      text: 'Maak een account',
+                      validator: _emailValidator,
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  const SizedBox(
+                    height: verticalSpacing1,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: TextFormField(
+                      controller: passwordController,
+                      textInputAction: TextInputAction.done,
+                      decoration: const InputDecoration(
+                        labelText: 'Wachtwoord',
+                      ),
+                      obscureText: true,
+                      validator: _passwordValidator,
+                    ),
+                  ),
+                  const SizedBox(height: verticalSpacing2),
+                  BlackButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        signin(context, emailController, passwordController);
+                      }
+                    },
+                    text: 'inloggen',
+                  ),
+                  CustomTextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const ForgotPasswordPage(),
+                        ),
+                      );
+                    },
+                    text: 'Wachtwoord resetten',
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('geen account?'),
+                      CustomTextButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => const RegisterPage()),
+                          );
+                        },
+                        text: 'Maak een account',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
