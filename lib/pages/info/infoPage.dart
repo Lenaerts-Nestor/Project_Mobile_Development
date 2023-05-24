497fc08bd5163538061ab8f81792bc4a2d16208b
Kevin Van Rooy <kevinvanrooy96@hotmail.com>
2023-05-24 14:35:14 +0200

    style veranderingen

--- lib/pages/info/infoPage.dart
+++ lib/pages/info/infoPage.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: file_names
 
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:intl/intl.dart';
 import 'package:parkflow/components/custom_appbar.dart';
 import 'package:parkflow/components/style/designStyle.dart';
@@ -12,6 +15,7 @@ import 'package:parkflow/model/user/user_logged_controller.dart';
 import 'package:rxdart/rxdart.dart';
 
 import '../map/functions/markers/marker.dart';
+import '../map/functions/markers/marker_functions.dart';
 
 class InfoPage extends StatefulWidget {
   const InfoPage({Key? key}) : super(key: key);
@@ -23,18 +27,44 @@ class InfoPage extends StatefulWidget {
 class _InfoPageState extends State<InfoPage> {
   late double getlatidude;
   late double getlotitude;
+  late Stream<QuerySnapshot> parkedUserStream;
+  late Stream<QuerySnapshot> reservedUserStream;
+  late Timer _timer;
+  List<Marker> _markers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    final userLogged = Provider.of<UserLogged>(context, listen: false);
+    final currentUserId = userLogged.email.trim();
+
+    parkedUserStream = FirebaseFirestore.instance
+        .collection('markers')
+        .where('parkedUserId', isEqualTo: currentUserId)
+        .snapshots();
+
+    reservedUserStream = FirebaseFirestore.instance
+        .collection('markers')
+        .where('reservedUserId', isEqualTo: currentUserId)
+        .snapshots();
+    _timer = Timer.periodic(const Duration(seconds: 1), (Timer timer) async {
+      updateMarkerState();
+      _updateMarkers();
+    });
+  }
+
+  void _updateMarkers() {
+    getMarkersFromDatabase(context, (List<Marker> markers) {
+      setState(() {
+        _markers = markers;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final userLogged = Provider.of<UserLogged>(context);
     final currentUserId = userLogged.email.trim();
-
-    final parkedUserQuery = FirebaseFirestore.instance
-        .collection('markers')
-        .where('parkedUserId', isEqualTo: currentUserId);
-
-    final reservedUserQuery = FirebaseFirestore.instance
-        .collection('markers')
-        .where('reservedUserId', isEqualTo: currentUserId);
 
     return Scaffold(
       appBar: MyAppBar(
@@ -43,117 +73,127 @@ class _InfoPageState extends State<InfoPage> {
         marginleft: 0,
         onPressed: () {},
       ),
-      body: StreamBuilder<List<QuerySnapshot>>(
-        stream: Rx.combineLatest2(
-          parkedUserQuery.snapshots(),
-          reservedUserQuery.snapshots(),
-          (QuerySnapshot a, QuerySnapshot b) => [a, b],
-        ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: parkedUserStream,
         builder: (BuildContext context,
-            AsyncSnapshot<List<QuerySnapshot>> snapshot) {
-          if (snapshot.hasError) {
+            AsyncSnapshot<QuerySnapshot> parkedSnapshot) {
+          if (parkedSnapshot.hasError) {
             return const Text('Something went wrong');
           }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
+          if (parkedSnapshot.connectionState == ConnectionState.waiting) {
             return const CircularProgressIndicator();
           }
 
-          final allDocs = [
-            ...snapshot.data![0].docs,
-            ...snapshot.data![1].docs,
-          ];
+          return StreamBuilder<QuerySnapshot>(
+            stream: reservedUserStream,
+            builder: (BuildContext context,
+                AsyncSnapshot<QuerySnapshot> reservedSnapshot) {
+              if (reservedSnapshot.hasError) {
+                return const Text('Something went wrong');
+              }
 
-          final markers = allDocs.map((doc) {
-            final markerData = doc.data() as Map<String, dynamic>;
-            markerData['id'] = doc.id;
-            getlatidude = markerData['latitude'];
-            getlotitude = markerData['longitude'];
-            return MarkerInfo.fromJson(markerData);
-          }).toList();
+              if (reservedSnapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
 
-          if (markers.isEmpty) {
-            return const Padding(
-              padding: EdgeInsets.all(padding),
-              child: Center(
-                child: Text(
-                  'U heeft momenteel geen voertuigen in gebruik. U kunt parkeren en reserveren op de kaart.',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            );
-          }
+              final allDocs = [
+                ...parkedSnapshot.data!.docs,
+                ...reservedSnapshot.data!.docs,
+              ];
 
-          return Padding(
-            padding: const EdgeInsets.all(padding),
-            child: ListView.builder(
-              itemCount: markers.length,
-              itemBuilder: (context, index) {
-                final marker = markers[index];
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: verticalSpacing1),
-                  child: Container(
-                    height: marker.reservedUserId == '' ? 100 : 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      border: Border.all(color: Colors.black12),
-                      color: color2,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ListTile(
-                          isThreeLine: true,
-                          title: Text(
-                              'Parked Vehicle: ${marker.parkedVehicleBrand}'),
-                          subtitle: marker.reservedUserId == ''
-                              ? Text(
-                                  'From: ${formatDateTime(marker.startTime)} - To: ${formatDateTime(marker.endTime)}')
-                              : Text(
-                                  'From: ${formatDateTime(marker.startTime)} - To: ${formatDateTime(marker.prevEndTime)}'),
-                          trailing: SizedBox(
-                            width: 50,
-                            height: 50,
-                            child: getSvg(marker.parkedVehicleBrand,
-                                getColor(marker.parkedVehicleColor)),
-                          ),
-                          onTap: userLogged.email == marker.parkedUserId &&
-                                  marker.reservedUserId == "" &&
-                                  marker.reservedUserId != userLogged.email
-                              ? () {
-                                  showPopupEdit(
-                                      context, marker, userLogged.email);
-                                }
-                              : () {
-                                  // Show a message indicating that you cannot make changes
-                                },
-                        ),
-                        if (marker.reservedUserId != '')
-                          ListTile(
-                            isThreeLine: true,
-                            title: Text(
-                                'Reserved Vehicle: ${marker.reservedVehicleBrand}'),
-                            subtitle: Text(
-                                'From: ${formatDateTime(marker.prevEndTime)} - To: ${formatDateTime(marker.endTime)}'),
-                            trailing: SizedBox(
-                              width: 50,
-                              height: 50,
-                              child: getSvg(marker.reservedVehicleBrand,
-                                  getColor(marker.reservedVehicleColor)),
-                            ),
-                            onTap: userLogged.email == marker.reservedUserId
-                                ? () {
-                                    showPopupEdit(
-                                        context, marker, userLogged.email);
-                                  }
-                                : () {},
-                          ),
-                      ],
+              final markers = allDocs.map((doc) {
+                final markerData = doc.data() as Map<String, dynamic>;
+                markerData['id'] = doc.id;
+                getlatidude = markerData['latitude'];
+                getlotitude = markerData['longitude'];
+                return MarkerInfo.fromJson(markerData);
+              }).toList();
+
+              if (markers.isEmpty) {
+                return const Padding(
+                  padding: EdgeInsets.all(padding),
+                  child: Center(
+                    child: Text(
+                      'U heeft momenteel geen voertuigen in gebruik. U kunt parkeren en reserveren op de kaart.',
+                      textAlign: TextAlign.center,
                     ),
                   ),
                 );
-              },
-            ),
+              }
+
+              return Padding(
+                padding: const EdgeInsets.all(padding),
+                child: ListView.builder(
+                  itemCount: markers.length,
+                  itemBuilder: (context, index) {
+                    final marker = markers[index];
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: verticalSpacing1),
+                      child: Container(
+                        height: marker.reservedUserId == '' ? 100 : 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          border: Border.all(color: Colors.black12),
+                          color: color2,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ListTile(
+                              isThreeLine: true,
+                              title: Text(
+                                  'Parked Vehicle: ${marker.parkedVehicleBrand}'),
+                              subtitle: marker.reservedUserId == ''
+                                  ? Text(
+                                      'From: ${formatDateTime(marker.startTime)} - To: ${formatDateTime(marker.endTime)}')
+                                  : Text(
+                                      'From: ${formatDateTime(marker.startTime)} - To: ${formatDateTime(marker.prevEndTime)}'),
+                              trailing: SizedBox(
+                                width: 50,
+                                height: 50,
+                                child: getSvg(marker.parkedVehicleBrand,
+                                    getColor(marker.parkedVehicleColor)),
+                              ),
+                              onTap: userLogged.email == marker.parkedUserId &&
+                                      marker.reservedUserId == '' &&
+                                      marker.reservedUserId != userLogged.email
+                                  ? () {
+                                      showPopupEdit(
+                                          context, marker, userLogged.email);
+                                    }
+                                  : () {
+                                      // Show a message that you cannot make changes
+                                    },
+                            ),
+                            if (marker.reservedUserId != '')
+                              ListTile(
+                                isThreeLine: true,
+                                title: Text(
+                                    'Reserved Vehicle: ${marker.reservedVehicleBrand}'),
+                                subtitle: Text(
+                                    'From: ${formatDateTime(marker.prevEndTime)} - To: ${formatDateTime(marker.endTime)}'),
+                                trailing: SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: getSvg(marker.reservedVehicleBrand,
+                                      getColor(marker.reservedVehicleColor)),
+                                ),
+                                onTap: userLogged.email == marker.reservedUserId
+                                    ? () {
+                                        showPopupEdit(
+                                            context, marker, userLogged.email);
+                                      }
+                                    : () {},
+                              ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
           );
         },
       ),
